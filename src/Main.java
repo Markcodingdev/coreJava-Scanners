43513e99f765aea499d03b8e3ca47df8601f67ea
Mark Mckevitt <mark@Marks-MBP.lan>
2022-09-11 10:38:49 -0700

    first lessons

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int x = 5;
+        int y = 10;
+        System.out.println(x + y);
     }
+
+
 }
